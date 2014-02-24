d871fdbb5e9301ae2338729d0c0764bab69cef70
xif6 <francois.pasini@gmail.com>
2014-02-24 18:29:32 +0100

    create branch

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,13 +16,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Acme\HelloBundle\AcmeHelloBundle(),
-            new Acme\StoreBundle\AcmeStoreBundle(),
-            new Acme\BlogBundle\AcmeBlogBundle(),
-            new Acme\TaskBundle\AcmeTaskBundle(),
-			new Acme\SecurityBundle\AcmeSecurityBundle(),
-			new FOS\UserBundle\FOSUserBundle(),
-            new Acme\UserBundle\AcmeUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
--- src/Acme/SecurityBundle/AcmeSecurityBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Acme\SecurityBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AcmeSecurityBundle extends Bundle
-{
-}
