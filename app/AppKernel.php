75d481785b4980b98ff36cd37b78a55c557936df
François PASINI <xif6@xif6.com>
2014-03-26 16:11:57 +0100

    suppr dir app/app wrong copy

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,8 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-			new Xif6\NewsrssBundle\Xif6NewsrssBundle(),
-			new FOS\UserBundle\FOSUserBundle(),
+            new Xif6\NewsrssBundle\Xif6NewsrssBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
