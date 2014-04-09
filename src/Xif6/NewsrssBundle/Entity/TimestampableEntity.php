8bafe6428811ef7f76644839c9ba33386153a5ee
xif6 <francois.pasini@gmail.com>
2014-04-09 20:18:36 +0200

    use trait for create_at update_at

---
+++ src/Xif6/NewsrssBundle/Entity/TimestampableEntity.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Xif6\NewsrssBundle\Entity;
+
+/**
+ * Timestampable Trait, usable with PHP >= 5.4
+ */
+trait TimestampableEntity
+{
+    /**
+     * @ORM\Column(name="created_at", type="datetime", nullable=true)
+     * @Gedmo\Timestampable(on="create")
+     */
+    protected $createdAt;
+
+    /**
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true)
+     * @Gedmo\Timestampable(on="update")
+     */
+    protected $updatedAt;
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     * @return $this
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     * @return $this
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return DateTime
+     */
+    public function getUpdated()
+    {
+        return $this->updatedAt;
+    }
+}
