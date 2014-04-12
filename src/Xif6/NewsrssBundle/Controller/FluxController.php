e4e3268156d979cf33fc8b7e38653387d72ad6f0
xif6 <francois.pasini@gmail.com>
2014-04-12 13:31:22 +0200

    add roote xif6_newss_flux_items

--- src/Xif6/NewsrssBundle/Controller/FluxController.php
+++ src/Xif6/NewsrssBundle/Controller/FluxController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Xif6\NewsrssBundle\Entity\Flux;
+use Xif6\NewsrssBundle\Entity\UserFlux;
 
 class FluxController extends Controller
 {
@@ -24,5 +25,28 @@ class FluxController extends Controller
         var_dump($flux->getSite());
 
         return array('flux' => $flux);
+    }
+
+    /**
+     * @Template()
+     */
+    public function itemsAction($id, $type)
+    {
+        //*
+        if ($type == 'user' && $this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $userFlux = $this->getDoctrine()
+                ->getrepository('Xif6NewsrssBundle:UserFlux')
+                ->findOneBy(array('flux' => $id, 'user' => $this->getUser()->getId()));
+            $item_nb = $userFlux->getFluxNb();
+        } else {
+            $userFlux = new UserFlux();
+        }
+        //*/
+        $items = $this->get('doctrine_mongodb')
+            ->getrepository('Xif6NewsrssBundle:Item')
+            ->findBy(array('flux_id' => (int)$id), array('date' => 'DESC'), $userFlux->getFluxNb());
+        //*/
+
+        return array('items' => $items, 'userFlux' => $userFlux);
     }
 }
