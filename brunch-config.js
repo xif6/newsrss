abbfafa2b8669391d6461f808166c204bc023dfa
xif6 <francois.pasini@gmail.com>
2016-05-29 01:55:45 +0200

    use npm bower brunch for static

---
+++ brunch-config.js
@@ -0,0 +1,25 @@
+'use strict';
+
+exports.config = {
+    paths: {
+        'public': 'web/assets',
+        'watched': ['app/Resources/public', 'src/NewsrssBundle/Resources/public']
+    },
+    conventions: {
+        'assets': /Resources\/public\/assets/
+    },
+    files: {
+        javascripts: {
+            joinTo: {
+                'js/app.js': /^(?!bower_components)/,
+                'js/vendor.js': /^bower_components/
+            }
+        },
+        stylesheets: {
+            joinTo: {
+                'css/styles.css': /^(?!bower_components)/,
+                'css/vendor.css': /^bower_components/
+            }
+        }
+    }
+};
