0c70721e20f79de0e167555eb77c8fcb9ea357f4
xif6 <francois.pasini@gmail.com>
2016-06-01 19:34:51 +0200

    test feedreader

--- src/NewsrssBundle/Command/FeedReaderCommand.php
+++ src/NewsrssBundle/Command/FeedReaderCommand.php
@@ -7,6 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Filesystem\Filesystem;
+use NewsrssBundle\Document\Item;
 
 class FeedReaderCommand extends ContainerAwareCommand
 {
@@ -19,13 +20,40 @@ class FeedReaderCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        $this->em = $this->getContainer()->get('doctrine.orm.entity_manager');
         $reader = $this->getContainer()->get('debril.reader');
+        $itemRepository = $this->dm->getRepository('NewsrssBundle:Item');
 
         $url = 'http://www.clubic.com/articles.rss';
         $url = 'http://www.nextinpact.com/rss/news.xml';
+        $feed = $reader->getFeedContent($url);
+        var_dump($feed->getItems()[0]);
+        die();
+
+        //$all = $this->em->getRepository('NewsrssBundle:Flux')->findBy(['id' => 686], null/*, 500*/);
+        $allFlux = $this->em->getRepository('NewsrssBundle:Flux')->findAll();
+
+        foreach ($allFlux as $flux) {
+            $feed = $reader->getFeedContent($flux->getUrl());
+
+            foreach ($feed->getItems() as $itemRss) {
+                $item = new Item();
+                $item
+                    ->setTitle($itemRss->title)
+                    ->setDescription($itemRss->description)
+                    ->setCategory($itemRss->category)
+                    ->setUrl($itemRss->link)
+                    ->setDate($itemRss->date)
+                    ->setImage($itemRss->image)
+                    ->setFluxId($flux->getId());
+                $itemRepository->upsert($item, $flux->head->date);
+            }
+
+        }
 
         $feed = $reader->getFeedContent($url);
         var_dump($feed->getItems()[0]);
     }
+
 
 }
