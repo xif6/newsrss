71146fcaa55b708f4f797a6bd755c4bfb03c2690
xif6 <francois.pasini@gmail.com>
2017-02-22 16:55:54 +0100

    add JWT

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -25,6 +25,7 @@ class AppKernel extends Kernel
             new NewsrssBundle\NewsrssBundle(),
             new Csa\Bundle\GuzzleBundle\CsaGuzzleBundle(),
             new Debril\RssAtomBundle\DebrilRssAtomBundle(),
+            new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
