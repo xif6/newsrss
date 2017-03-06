d2cd0235c91c3e5957a50c8858d0f58eaeed3c8f
xif6 <francois.pasini@gmail.com>
2017-03-06 10:15:41 +0100

    add NelmioCorsBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
             new Csa\Bundle\GuzzleBundle\CsaGuzzleBundle(),
             new Debril\RssAtomBundle\DebrilRssAtomBundle(),
             new Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle(),
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
