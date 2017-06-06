284906297c25d6bc47553d77066e9e6d1ec7c690
fpasini <fpasini@kernix.com>
2017-06-06 14:38:23 +0200

    config uuid in entity

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -4,6 +4,7 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Timestampable\Traits\TimestampableEntity;
 
 /**
  * @ORM\Entity
